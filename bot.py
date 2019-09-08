e560d8ce056eebd1cb9d42ab62dfcae8ef0666f2
Jatin Mehta <exoceus@gmail.com>
2019-09-08 08:21:08 -0400

    first commit

---
+++ bot.py
@@ -0,0 +1,85 @@
+import sys
+import os
+import logging
+import asyncio
+
+from pykeybasebot import Bot
+import pykeybasebot.types.chat1 as chat1
+
+from scraper import *
+
+logging.basicConfig(level=logging.DEBUG)
+
+if "win32" in sys.platform:
+    # Windows specific event-loop policy
+    asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+
+
+class Handler:
+    async def __call__(self, bot, event):
+        # define trigger, other vars
+        trigger = event.msg.content.text.body[:5]
+        query = event.msg.content.text.body[6:]
+        channel = event.msg.channel
+
+        if event.msg.content.type_name != chat1.MessageTypeStrings.TEXT.value:
+            return
+
+        elif trigger == "/wiki":
+
+            summary, URL = scrape(query)
+
+            if summary == " ":
+                output = '*Error*: the query you entered was either mis spelled or does not exists on wikipedia. Please try again with a valid query.'
+
+            else:
+                output = summary + "\n Learn more at: " + URL
+
+            await bot.chat.send(channel, output)
+
+        elif trigger == "/priv":
+            if query == "what\'s new?":
+                output = 'Good morning!\n:warning: There was a security break at Facebook.\nWould you like to know what personal data is at risk?'
+
+            elif query == "sure":
+                output = 'Facebook knows the following about you:\n1. Full name\n2. Email\n3. Birthday\n4. Location\n5. Friends list'
+
+            elif query == "yep":
+                output = ':speech_balloon:'
+
+            elif query == "who knows my real name?":
+                output = 'There are *28* services that know your full name, and *72* that know only your first name.\nWhich would you like to know more about?'
+
+            elif query == "full name":
+                output = 'Okay, here are the top 3 services that know your full name:\n1. Facebook\n2. Google\n3. Amazon\nWhat can I help you with?'
+
+            elif query == "delete my amazon account":
+                output = ':white_check_mark:It has been deleted.'
+
+            elif query == "thanks":
+                output = ':innocent:No worries. Have a good day!'
+
+            else:
+                output = 'Hi there! I am your fully autonomous digital assistant. Type /priv "message" to get started. Ask me "what\'s new?"'
+
+            await bot.chat.send(channel, output)
+
+
+listen_options = {
+    "local": True,
+    "wallet": True,
+    "dev": True,
+    "hide-exploding": False,
+    "filter_channel": None,
+    "filter_channels": None,
+}
+
+bot = Bot(
+    username="botnow", paperkey="deer apology urban celery fatigue black net report neck bulk return lounge gallery", handler=Handler()
+)
+
+asyncio.run(bot.start(listen_options))
+
+# Improvments:
+# -format for the 'most commonly refers to:'
+# - get rid of the [1], etc
---
+++ scraper.py
@@ -0,0 +1,43 @@
+import urllib
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+from urllib.error import HTTPError
+print('Dependencies loaded')
+
+
+def scrape(topic):
+    wiki_query = str(topic)
+    wiki_query = wiki_query.strip()
+
+    # format query for GET request to Wikipedia
+    wiki_query = wiki_query.replace(" ", "_")
+    wiki_URL = 'https://en.wikipedia.org/wiki/' + wiki_query
+
+    try:
+        uClient = uReq(wiki_URL)
+    except:
+        raw_paragraph = " "
+        return raw_paragraph, wiki_URL
+
+    page_soup = soup(uClient.read(), "html.parser")
+    uClient.close()
+
+    for p in page_soup.find_all("p", {'class': 'mw-empty-elt'}):
+        p.decompose()
+
+    raw_paragraph = page_soup.find("div", {"class": "mw-parser-output"})
+    raw_paragraph = raw_paragraph.p
+    raw_paragraph = raw_paragraph.get_text()
+
+    if raw_paragraph[-25:-1] == 'most commonly refers to:':
+        options = page_soup.find_all("ul")[0]
+        options = options.find_all('li')
+
+        for i in range(len(options)):
+            options[i] = options[i].get_text()
+            options[i] = '\n- ' + options[i]
+            raw_paragraph += options[i]
+
+    print(raw_paragraph)
+
+    return raw_paragraph, wiki_URL
